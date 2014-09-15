f44b6d050795157f476519b6a1647b8260e8722c
Justin <justin.tyler.coon@gmail.com>
2014-09-15 14:47:01 -0400

    increased efficiency by storing size * .002 in each dott

--- dotts.js
+++ dotts.js
@@ -9,15 +9,15 @@ var dotts = [];
 var mouseX = 500;
 var mouseY = 500;
 for(var i = 0; i < num; i++) {
-	dotts[i] = {x:mouseX,  y:mouseY, velX:0, velY:0, size:(((i/num)*10)+10)*.002};
+	dotts[i] = {x:mouseX,  y:mouseY, velX:0, velY:0, size:((i/num)*10)+10, affectability:(((i/num)*10)+10)*.002};
 }
 function draw() {
 	context.clearRect(0, 0, window.innerWidth, window.innerHeight);
 	for(var i = 0; i < dotts.length; i++) {
 		var dott = dotts[i];
-		context.fillRect(dott.x, dott.y, dott.size*500, dott.size*500);
-		dott.x += dott.velX*dott.size;
-		dott.y += dott.velY*dott.size;
+		context.fillRect(dott.x, dott.y, dott.size, dott.size);
+		dott.x += dott.velX*dott.affectability;
+		dott.y += dott.velY*dott.affectability;
 		dott.velX += mouseX-dott.x;
 		dott.velY += mouseY-dott.y; 
 	}
